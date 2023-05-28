7291815e74353fc91f51d343b9244949c18c5bca
Jean Manuel <jeansmaa@gmail.com>
2023-05-28 19:59:35 -0400

    Add project files.

---
+++ WindowsApplication3/Main.Designer.cs
@@ -0,0 +1,199 @@
+﻿
+using DevExpress.XtraGrid;
+namespace DXSample {
+    partial class Main {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.ribbonPageGroup1 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.defaultLookAndFeel1 = new DevExpress.LookAndFeel.DefaultLookAndFeel(this.components);
+            this.recordBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.gridControl2 = new DevExpress.XtraGrid.GridControl();
+            this.gridView2 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.colID = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colParentID = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colText = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colDt = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colDt2 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colState = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colImage = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.recordBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ribbonPageGroup1
+            // 
+            this.ribbonPageGroup1.Name = "ribbonPageGroup1";
+            this.ribbonPageGroup1.Text = "ribbonPageGroup1";
+            // 
+            // defaultLookAndFeel1
+            // 
+            this.defaultLookAndFeel1.LookAndFeel.SkinName = "Office 2016 Colorful";
+            // 
+            // recordBindingSource
+            // 
+            this.recordBindingSource.DataSource = typeof(DXSample.Data.Record);
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.DataSource = this.recordBindingSource;
+            this.gridControl1.Location = new System.Drawing.Point(0, 0);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(462, 538);
+            this.gridControl1.TabIndex = 0;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.colID,
+            this.colParentID,
+            this.colText,
+            this.colDt,
+            this.colDt2,
+            this.colState,
+            this.colImage});
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            this.gridView1.OptionsSelection.MultiSelect = true;
+            // 
+            // gridControl2
+            // 
+            this.gridControl2.Location = new System.Drawing.Point(468, 0);
+            this.gridControl2.MainView = this.gridView2;
+            this.gridControl2.Name = "gridControl2";
+            this.gridControl2.Size = new System.Drawing.Size(462, 538);
+            this.gridControl2.TabIndex = 1;
+            this.gridControl2.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView2});
+            // 
+            // gridView2
+            // 
+            this.gridView2.GridControl = this.gridControl2;
+            this.gridView2.Name = "gridView2";
+            // 
+            // colID
+            // 
+            this.colID.FieldName = "ID";
+            this.colID.Name = "colID";
+            this.colID.Visible = true;
+            this.colID.VisibleIndex = 0;
+            // 
+            // colParentID
+            // 
+            this.colParentID.FieldName = "ParentID";
+            this.colParentID.Name = "colParentID";
+            this.colParentID.Visible = true;
+            this.colParentID.VisibleIndex = 1;
+            // 
+            // colText
+            // 
+            this.colText.FieldName = "Text";
+            this.colText.Name = "colText";
+            this.colText.Visible = true;
+            this.colText.VisibleIndex = 2;
+            // 
+            // colDt
+            // 
+            this.colDt.FieldName = "Dt";
+            this.colDt.Name = "colDt";
+            this.colDt.Visible = true;
+            this.colDt.VisibleIndex = 3;
+            // 
+            // colDt2
+            // 
+            this.colDt2.FieldName = "Dt2";
+            this.colDt2.Name = "colDt2";
+            this.colDt2.Visible = true;
+            this.colDt2.VisibleIndex = 4;
+            // 
+            // colState
+            // 
+            this.colState.FieldName = "State";
+            this.colState.Name = "colState";
+            this.colState.Visible = true;
+            this.colState.VisibleIndex = 5;
+            // 
+            // colImage
+            // 
+            this.colImage.FieldName = "Image";
+            this.colImage.Name = "colImage";
+            this.colImage.Visible = true;
+            this.colImage.VisibleIndex = 6;
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(343, 0);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(119, 33);
+            this.simpleButton1.TabIndex = 2;
+            this.simpleButton1.Text = "Copy Rows";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(936, 550);
+            this.Controls.Add(this.simpleButton1);
+            this.Controls.Add(this.gridControl2);
+            this.Controls.Add(this.gridControl1);
+            this.Name = "Main";
+            this.Text = "Main";
+            ((System.ComponentModel.ISupportInitialize)(this.recordBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource recordBindingSource;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup1;
+        private DevExpress.LookAndFeel.DefaultLookAndFeel defaultLookAndFeel1;
+        private GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.XtraGrid.Columns.GridColumn colID;
+        private DevExpress.XtraGrid.Columns.GridColumn colParentID;
+        private DevExpress.XtraGrid.Columns.GridColumn colText;
+        private DevExpress.XtraGrid.Columns.GridColumn colDt;
+        private DevExpress.XtraGrid.Columns.GridColumn colDt2;
+        private DevExpress.XtraGrid.Columns.GridColumn colState;
+        private DevExpress.XtraGrid.Columns.GridColumn colImage;
+        private GridControl gridControl2;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView2;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+    }
+}
---
+++ WindowsApplication3/Main.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using DevExpress.XtraBars.Docking2010.Views;
+using DevExpress.XtraEditors;
+using DXSample.Data;
+
+namespace DXSample {
+    public partial class Main : XtraForm {
+        BindingList<Record> records = new BindingList<Record>();
+        public Main() {
+            InitializeComponent();
+            recordBindingSource.DataSource = DataHelper.GetData(20, 10);
+
+            gridControl2.DataSource = records;
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e) {
+            int[] selectedRows = gridView1.GetSelectedRows();
+            foreach (int index in selectedRows) {
+                Record record = gridView1.GetRow(index) as Record;
+                Record newRecord = new Record();
+                newRecord.ID = record.ID;
+                newRecord.Image = record.Image;
+                newRecord.ParentID = record.ParentID;
+                newRecord.State = record.State;
+                newRecord.TestDetails = record.TestDetails;
+                newRecord.Text = record.Text;
+
+                records.Add(record);
+            }
+        }
+    }
+}
